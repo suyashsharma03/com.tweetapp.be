f4101f2dddba5b73590f42f523b5b3465e678720
SUYASH SHARMA <suyash03041996@gmail.com>
2022-07-24 19:56:50 +0530

    added user control and coressponding packages

---
+++ com.tweetapp/Mapper/UserDataMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using com.tweetapp.Models;
+using com.tweetapp.Models.Dtos.UserDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.tweetapp.Mapper
+{
+    public class UserDataMapper : Profile
+    {
+        public UserDataMapper()
+        {
+            CreateMap<CreateUserDto, Users>().ReverseMap();
+            CreateMap<ViewUserDto, Users>().ReverseMap();
+            CreateMap<ViewUserDto, CreateUserDto>().ReverseMap();
+
+            //CreateMap<CreateTweetDto, Tweets>();
+            
+        }
+    }
+}
--- com.tweetapp/Startup.cs
+++ com.tweetapp/Startup.cs
@@ -1,15 +1,16 @@
+using com.tweetapp.Data;
+using com.tweetapp.Data.IRepository;
+using com.tweetapp.Mapper;
+using com.tweetapp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace com.tweetapp
 {
@@ -25,6 +26,28 @@ namespace com.tweetapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IMongoClient, MongoClient>(mongo => new MongoClient(Configuration.GetValue<string>("TweetAppDBSettings:ConnectionString")));
+            services.AddSingleton<IUserRepository, UserRepository>();
+            //services.AddSingleton<ITweetRepository, TweetRepository>();
+            services.AddSingleton<IUserService, UserService>();
+            //services.AddSingleton<ITweetService, TweetService>();
+            services.AddAutoMapper(typeof(UserDataMapper));
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("TweetAppAPI", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Tweet App API",
+                    Version = "1.0"
+                });
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //options.IncludeXmlComments(xmlPath);
+            });
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.WithOrigins("http://localhost:4200")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddControllers();
         }
 
@@ -37,6 +60,14 @@ namespace com.tweetapp
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/TweetAppAPI/swagger.json", "Tweet App API");
+                options.RoutePrefix = "";
+            });
 
             app.UseRouting();
 
